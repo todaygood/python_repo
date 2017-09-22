e9dc534d016658474ace0ee06d30b70895fcfdb6
Margin <jhu_com@163.com>
2017-09-22 13:43:15 +0800

    rename some file

---
+++ make_dict.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+
+#data={'red':1,'blue':2,'green':3}
+
+def makedict(**kwargs):
+    return kwargs
+
+data=makedict(red=1,blue=2,green=3)
+
+print(data)
+
+    
---
+++ safe_get_from_dict.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python
+
+d={'key':5}
+
+if d.has_key('key'):
+    print( d['key'])
+else:
+    print('no key')
+
+
+
+
+
