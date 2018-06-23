0cc9d963311532197c8e3572d55d315dc3a57bd5
Margin <jhu_com@163.com>
2018-06-24 02:49:08 +0800

    load,dump

--- basic/json/prettyprint_json.py
+++ basic/json/prettyprint_json.py
@@ -3,15 +3,20 @@
 import json,sys
 
 def main():
-   if (len(sys.argv)) < 2:
+    print("begin")
+    if (len(sys.argv)) < 2:  
        print("Usage:%s json_file"% sys.argv[0])
        return -1
    
-   with open(sys,argv[1],"r") as infile:
-       json = json.loads(infile) 
-       json.dumps(json,sort_keys=True,indent=4,s
+    with open(sys.argv[1],"r") as infile:
+       print("pretty print the json file")
+       js = json.load(infile) 
+       outfile = open("./nodes2.json","w")
+       json.dump(js,outfile,sort_keys=True,indent=4,separators=(',', ': '))
    
+    infile.close()
+    outfile.close()
 
 
-if "__line__" == "__main__":
+if __name__ == "__main__":
     main()
