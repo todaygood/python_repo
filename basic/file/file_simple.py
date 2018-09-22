0f0a42d739cf4f79fb9be7be046e6f33e061f155
Margin Hu <jhu_com@163.com>
2018-09-22 09:57:55 +0800

    traver each line

--- basic/file/file_simple.py
+++ basic/file/file_simple.py
@@ -1,13 +1,20 @@
 #!/usr/bin/env python
 
-fp = file('data.txt')
+fp = open('data.txt', 'r')
 lines = []
+
 for line in fp: # 内置的迭代器, 效率很高
+    print(line)
     lines.append(line)
-    fp.close()
+
+fp.close()
 
 lines.insert(1, 'a new line') # 在第二行插入
+
 s = '\n'.join(lines)
-fp = file('data.txt', 'w')
+
+fp = open('data2.txt', 'w')
+
 fp.write(s)
+
 fp.close()
